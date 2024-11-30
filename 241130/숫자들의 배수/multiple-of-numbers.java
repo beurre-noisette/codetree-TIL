c1b469b68a159bf30b69d4ad1f223535d20ad075
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2024-11-30 17:20:05 +0900

    Title: 숫자들의 배수, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241130/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int multiplicand = sc.nextInt();
+        int multiplier = 1;
+        int cnt = 0;
+
+        while (true) {
+            System.out.print((multiplicand * multiplier) + " ");
+
+            multiplier++;
+
+            if (multiplier % 5 == 0) {
+                cnt++;
+            }
+
+            if (cnt == 2) {
+                System.out.print((multiplicand * multiplier) + " ");
+                break;
+            }
+        }
+
+        sc.close();
+    }
+}
