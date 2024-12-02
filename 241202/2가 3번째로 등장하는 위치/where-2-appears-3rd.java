2c8a23d85f398971664abae17ed76b81bdf861d3
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2024-12-02 21:18:31 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241202/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int cnt = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                cnt++;
+
+                if (cnt == 3) {
+                    System.out.print(i + 1);
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
