a8787a0bd055f14b85d08eeb0412560f0bc784af
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2024-12-03 20:37:14 +0900

    Title: n개의 숫자 중 최대 2개, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241203/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int temp = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.print(arr[0] + " " + arr[1]);
+
+        sc.close();
+    }
+}
+
+/*
+* N개의 정수를 내림차순으로 정렬했을 때 첫 번째와 두 번째 숫자를 출력하기
+*/
