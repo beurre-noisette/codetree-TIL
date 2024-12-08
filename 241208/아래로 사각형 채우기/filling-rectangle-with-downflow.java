2bd0220e2a8658481173beb5db1431d6d5b9f514
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2024-12-08 19:13:10 +0900

    Title: 아래로 사각형 채우기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241208/아래로 사각형 채우기/filling-rectangle-with-downflow.java
+++ 241208/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                arr2d[i][j] = (1 + i) + (j * n);
+                arr2d[j][i] = num++;
             }
         }
 
