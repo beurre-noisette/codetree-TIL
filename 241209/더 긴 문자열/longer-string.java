42c6a956bef0778a4932d5993763cdc06b02e17b
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2024-12-09 20:17:56 +0900

    Title: 더 긴 문자열, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241209/더 긴 문자열/longer-string.java
+++ 241209/더 긴 문자열/longer-string.java
@@ -7,7 +7,13 @@ public class Main {
         String str1 = sc.next();
         String str2 = sc.next();
 
-        System.out.println((str1.length() > str2.length()) ? str1 + " " + str1.length() : str2 + " " + str2.length());
+        if (str1.length() > str2.length()) {
+            System.out.println(str1 + " " + str1.length());
+        } else if (str1.length() < str2.length()) {
+            System.out.println(str2 + " " + str2.length());
+        } else {
+            System.out.println("same");
+        }
 
         sc.close();
     }
