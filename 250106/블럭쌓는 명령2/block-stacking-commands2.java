7bcd8b30426ebd8103e5158e0bcf9ec9e511ff82
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2025-01-06 21:12:37 +0900

    Title: 블럭쌓는 명령2, Time: 123ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250106/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 전체 칸 수
+        int k = sc.nextInt(); // 명령의 수
+
+        int[] blocks = new int[n];
+
+        for (int i = 1; i <= k; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for (int j = a; j <= b; j++) {
+                blocks[j] += 1;
+            }
+        }
+
+        Arrays.stream(blocks).max().ifPresent(System.out::println);
+
+        sc.close();
+    }
+}
+
+/*
+(1) 1번 칸부터 N번째 칸까지 순서대로 총 N개의 칸이 있다.
+(2) 이중 Ai 번째 칸부터 Bi 번째 칸까지 각각 블럭을 1개씩 쌓으라는 명령이 총 K번 주어진다.
+(3) 명령을 다 수행한 이후 1번째 칸부터 N번째 칸까지 쌓인 블럭의 수 중 최댓값을 출력하시오.
+*/
