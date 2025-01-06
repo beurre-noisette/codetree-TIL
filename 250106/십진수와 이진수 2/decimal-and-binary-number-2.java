1d6948b60f982eefec68854aa8d264eceed1a5fc
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2025-01-06 20:30:04 +0900

    Title: 십진수와 이진수 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250106/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String binary = sc.next();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        decimal *= 17;
+
+        System.out.println(Integer.toBinaryString(decimal));
+
+        sc.close();
+    }
+}
