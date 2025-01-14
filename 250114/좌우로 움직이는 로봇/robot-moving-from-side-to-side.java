7c46b976a286d4ee533dcd4b80f23b38f1568d85
Sangwon Seo <150782681+beurre-noisette@users.noreply.github.com>
2025-01-14 20:46:13 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250114/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250114/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,68 +1,93 @@
-import java.util.Scanner;
-import java.util.Map;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         
-        Map<Integer, Integer> robotA = new TreeMap<>();
-        Map<Integer, Integer> robotB = new TreeMap<>();
+        int[][] robotA = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            robotA[i][0] = Integer.parseInt(st.nextToken());
+            robotA[i][1] = st.nextToken().equals("R") ? 1 : -1;
+        }
         
-        int posA = 0;
-        int timeA = 0;
-        robotA.put(0, 0);
+        int[][] robotB = new int[m][2];
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            robotB[i][0] = Integer.parseInt(st.nextToken());
+            robotB[i][1] = st.nextToken().equals("R") ? 1 : -1;
+        }
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        for (int i = 0; i < n; i++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
-            int move = (d == 'L') ? -1 : 1;
-            
-            for (int j = 0; j < t; j++) {
-                timeA++;
-                posA += move;
-                robotA.put(timeA, posA);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+테스트케이스
+
+
+결과
+
+
+코드 실행
+
+
+제출 및 채점
+
+
+
+
+        int totalTimeA = 0;
+        int totalTimeB = 0;
+        for(int i = 0; i < n; i++) totalTimeA += robotA[i][0];
+        for(int i = 0; i < m; i++) totalTimeB += robotB[i][0];
+        
+        int maxTime = Math.max(totalTimeA, totalTimeB);
+        int[] posA = new int[maxTime + 1];
+        int[] posB = new int[maxTime + 1];
+        
+        int currentPos = 0;
+        int timeIndex = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < robotA[i][0]; j++) {
+                currentPos += robotA[i][1];
+                posA[timeIndex++] = currentPos;
+            }
+        }
+        while(timeIndex <= maxTime) {
+            posA[timeIndex++] = currentPos;
+        }
+        
+        currentPos = 0;
+        timeIndex = 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < robotB[i][0]; j++) {
+                currentPos += robotB[i][1];
+                posB[timeIndex++] = currentPos;
+            }
+        }
+        while(timeIndex <= maxTime) {
+            posB[timeIndex++] = currentPos;
+        }
+        
+        int answer = 0;
+        for(int i = 1; i < maxTime; i++) {
+            if(posA[i] != posB[i] && posA[i+1] == posB[i+1]) {
+                answer++;
             }
         }
         
-        int posB = 0;
-        int timeB = 0;
-        robotB.put(0, 0);
-        
-        for (int i = 0; i < m; i++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
-            int move = (d == 'L') ? -1 : 1;
-            
-            for (int j = 0; j < t; j++) {
-                timeB++;
-                posB += move;
-                robotB.put(timeB, posB);
-            }
-        }
-        
-        int lastPosA = robotA.get(timeA);
-        int lastPosB = robotB.get(timeB);
-        
-        int count = 0;
-        int maxTime = Math.max(timeA, timeB);
-        
-        for (int time = 1; time <= maxTime; time++) {
-            int currA = time <= timeA ? robotA.get(time) : lastPosA;
-            int currB = time <= timeB ? robotB.get(time) : lastPosB;
-            int prevA = time - 1 <= timeA ? robotA.get(time - 1) : lastPosA;
-            int prevB = time - 1 <= timeB ? robotB.get(time - 1) : lastPosB;
-            
-            if (prevA != prevB && currA == currB) {
-                count++;
-            }
-        }
-        
-        System.out.println(count);
-        sc.close();
+        System.out.println(answer);
     }
 }
-
